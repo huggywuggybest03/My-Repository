a5a29e8118d66c0289f66568907b0146a1517167
monkey_kid03 <135007348+huggywuggybest03@users.noreply.github.com>
2023-07-06 18:15:50 +0100

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,54 @@
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/huggywuggylogi03/My-Repository/main"))()
+
+local GUI = Mercury:Create{
+    Name = "Test",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+
+GUI:Notification{
+	Title = "GUI",
+	Text = "Loaded GUI! Thanks for using this hub!",
+	Duration = 3,
+	Callback = function() end
+}
+
+GUI:Credit{
+	Name = "user",
+	Description = "desc",
+	Discord = "user here"
+}
+
+local HomeTab = GUI:Tab{
+	Name = "🏠 | Home",
+	Icon = "rbxassetid://8569322835"
+}
+
+HomeTab:Button{
+	Name = "Button",
+	Description = nil,
+	Callback = function() 
+        --code
+    end
+}
+
+HomeTab:Toggle{
+	Name = "Toggle",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state) 
+        if state then
+            --code
+        elseif not state then
+            --code
+        end
+    end
+}
+
+HomeTab:Textbox{
+	Name = "Textbox",
+	Callback = function(text) 
+        print(text)
+    end
+}
